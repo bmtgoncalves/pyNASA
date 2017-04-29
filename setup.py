14b92717c01fd2b0cc1b9d5adeea30212f3f757e
Brian Muhia <poppingtonic@gmail.com>
2017-04-29 14:07:14 +0300

    Include resources.json package data

--- setup.py
+++ setup.py
@@ -87,9 +87,9 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={ 
-        'pyNASA': ['pyNASA/resources.json'],
-    },
+    data_files=[
+        ('resources', ['pyNASA/resources.json']),
+    ],
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
